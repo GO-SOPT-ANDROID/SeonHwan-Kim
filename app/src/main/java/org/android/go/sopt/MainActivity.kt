7c6868b1ea98e7b2800a54214837b5bfaffa0b79
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-04-05 18:11:46 +0900

    feat : main page

--- app/src/main/java/org/android/go/sopt/MainActivity.kt
+++ app/src/main/java/org/android/go/sopt/MainActivity.kt
@@ -1,11 +1,27 @@
 package org.android.go.sopt
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.android.go.sopt.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var name: String
+    private lateinit var specialty: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        this.getMyData()
+    }
+
+    private fun getMyData() {
+        name = intent.getStringExtra("name").toString()
+        specialty = intent.getStringExtra("specialty").toString()
+        binding.tvMainName.text = "이름 : $name"
+        binding.tvMainSpecialty.text = "특기 : $specialty"
     }
 }
