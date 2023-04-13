5ad8160a2281dfb7ba629ed2bc21b1674cb1e90a
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-04-13 19:15:52 +0900

    fix : user data class 변경 #2

---
+++ app/src/main/java/org/android/go/sopt/presentation/main/MainActivity.kt
@@ -0,0 +1,63 @@
+package org.android.go.sopt.presentation.main
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import org.android.go.sopt.GalleryFragment
+import org.android.go.sopt.HomeFragment
+import org.android.go.sopt.R
+import org.android.go.sopt.SearchFragment
+import org.android.go.sopt.data.User
+import org.android.go.sopt.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        val currentFragment = supportFragmentManager.findFragmentById(R.id.fcv_main)
+//        if (currentFragment == null) {
+//            supportFragmentManager.beginTransaction().add(R.id.fcv_main, HomeFragment()).commit()
+//        }
+//        binding.bnvMain.setOnItemSelectedListener { item ->
+//            when (item.itemId) {
+//                R.id.menu_home -> {
+//                    HomeFragment()
+//                    return@setOnItemSelectedListener true
+//                }
+//                R.id.menu_search -> {
+//                    SearchFragment()
+//                    return@setOnItemSelectedListener true
+//                }
+//                else -> {
+//                    GalleryFragment()
+//                    return@setOnItemSelectedListener true
+//
+//                }
+//            }
+//        }
+
+        this.getUserData()
+    }
+
+    private fun getUserData() {
+        val user: User? = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            intent.getParcelableExtra("user", User::class.java)
+        } else{
+            intent.getParcelableExtra("user")
+        }
+        binding.tvMainName.text = "이름 : ${user?.name}"
+        binding.tvMainSpecialty.text = "특기 : ${user?.specialty}"
+    }
+
+//    private fun changeFragment(fragment: Fragment) {
+//        supportFragmentManager
+//            .beginTransaction()
+//            .replace(R.id.fcv_main, fragment)
+//            .commit()
+//    }
+}
