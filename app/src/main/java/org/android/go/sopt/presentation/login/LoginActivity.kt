cb037d20f4c48168a98c5aa6369d1646660ad166
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-04-13 16:54:10 +0900

    fix : getParcelableExtra #2

--- app/src/main/java/org/android/go/sopt/presentation/login/LoginActivity.kt
+++ app/src/main/java/org/android/go/sopt/presentation/login/LoginActivity.kt
@@ -1,11 +1,15 @@
 package org.android.go.sopt.presentation.login
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import org.android.go.sopt.R
+import org.android.go.sopt.data.User
 import org.android.go.sopt.presentation.main.MainActivity
 import org.android.go.sopt.presentation.signup.SignUpActivity
 import org.android.go.sopt.databinding.ActivityLoginBinding
@@ -17,11 +21,13 @@ import org.android.go.sopt.util.showShortToast
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
-    private lateinit var id: String
-    private lateinit var password: String
-    private lateinit var name: String
-    private lateinit var specialty: String
+//    private lateinit var id: String
+//    private lateinit var password: String
+//    private lateinit var name: String
+//    private lateinit var specialty: String
+    private var user: User? = null
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
@@ -31,42 +37,49 @@ class LoginActivity : AppCompatActivity() {
             hideKeyboard(binding.root)
         }
 
-        this.onClickLogin()
+//        this.onClickLogin()
         this.onCLickSignUp()
     }
 
-    private fun onClickLogin() {
-        binding.btMainLogin.setOnClickListener {
-            if (id == binding.etMainId.text.toString() && password == binding.etMainPassword.text.toString()) {
-                val intent = Intent(this, MainActivity::class.java)
-                intent.putExtra("name", name)
-                intent.putExtra("specialty", specialty)
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent)
-                showShortToast(getString(R.string.login_success_login_msg))
-            } else {
-                showShortToast(getString(R.string.login_fail_login_msg))
-            }
-        }
-    }
+//    private fun onClickLogin() {
+//        with(binding){
+//        btMainLogin.setOnClickListener {
+//                if (id == etMainId.text.toString() && password == etMainPassword.text.toString()) {
+//                    val intent = Intent(this@LoginActivity, MainActivity::class.java)
+//                    intent.putExtra("name", name)
+//                    intent.putExtra("specialty", specialty)
+//                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                    startActivity(intent)
+//                    showShortToast(getString(R.string.login_success_login_msg))
+//                    if(!isFinishing) finish()
+//                } else {
+//                    showShortToast(getString(R.string.login_fail_login_msg))
+//                }
+//            }
+//        }
+//    }
 
-    private fun onCLickSignUp() {
-        binding.btMainSignUp.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
-            getResultSignUp.launch(intent)
-        }
-    }
-
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     private val getResultSignUp = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
     ) { result: ActivityResult ->
         if (result.resultCode == RESULT_OK) {
-            id = result.data?.getStringExtra("id") ?: ""
-            password = result.data?.getStringExtra("password") ?: ""
-            name = result.data?.getStringExtra("name") ?: ""
-            specialty = result.data?.getStringExtra("specialty") ?: ""
+            user = result.data?.getParcelableExtra("user", User::class.java)
+            Log.d("user", "---------\n$user")
+//            id = result.data?.getStringExtra("id") ?: ""
+//            password = result.data?.getStringExtra("password") ?: ""
+//            name = result.data?.getStringExtra("name") ?: ""
+//            specialty = result.data?.getStringExtra("specialty") ?: ""
             showShortSnackbar(binding.root, getString(R.string.login_success_sign_up_msg))
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun onCLickSignUp() {
+        binding.btMainSignUp.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            getResultSignUp.launch(intent)
         }
     }
 }
