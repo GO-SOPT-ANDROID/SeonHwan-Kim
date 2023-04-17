853f1fa3d68a7eb93e9496db20a549e15ef80ce2
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-04-17 12:57:09 +0900

    add : home fragment #3

---
+++ app/src/main/java/org/android/go/sopt/HomeFragment.kt
@@ -0,0 +1,36 @@
+package org.android.go.sopt
+
+import MyAdapter
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import org.android.go.sopt.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding: FragmentHomeBinding
+        get() = requireNotNull(_binding) { "앗!_binding이 null이다!" }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.rvHomeGithubRepo.adapter = MyAdapter(requireContext())
+        binding.rvHomeGithubRepo.layoutManager = LinearLayoutManager(context)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
