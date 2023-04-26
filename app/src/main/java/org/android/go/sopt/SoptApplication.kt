c94507405413a0fb958eb24ecfcdf214a1884b8e
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-04-26 21:58:17 +0900

    add : sharedPreferences 분리 #2

---
+++ app/src/main/java/org/android/go/sopt/SoptApplication.kt
@@ -0,0 +1,15 @@
+package org.android.go.sopt
+
+import android.app.Application
+import org.android.go.sopt.util.UserSharedPreferences
+
+class SoptApplication : Application() {
+    companion object{
+        lateinit var prefs: UserSharedPreferences
+    }
+
+    override fun onCreate() {
+        prefs = UserSharedPreferences(applicationContext)
+        super.onCreate()
+    }
+}
