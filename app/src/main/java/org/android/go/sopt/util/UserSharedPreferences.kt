5027dacff1cb50ac4d37e7dfea1bb1a6120259b2
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-04-29 22:33:23 +0900

    fix : android ktx 사용 #3

--- app/src/main/java/org/android/go/sopt/util/UserSharedPreferences.kt
+++ app/src/main/java/org/android/go/sopt/util/UserSharedPreferences.kt
@@ -1,18 +1,21 @@
 package org.android.go.sopt.util
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.SharedPreferences
+import androidx.core.content.edit
 import org.android.go.sopt.data.User
 
 class UserSharedPreferences(context: Context) {
     private val prefs: SharedPreferences = context.getSharedPreferences(KEY_PREFS, 0)
 
+    // 안드로이드 ktx 사용
     fun getString(key: String, defValue: String?): String {
         return prefs.getString(key, defValue).toString()
     }
 
     fun setString(key: String, value: String?) {
-        prefs.edit().putString(key, value).apply()
+        prefs.edit { setString(key, value) }
     }
 
     fun getBoolean(key: String, defValue: Boolean): Boolean {
@@ -20,7 +23,7 @@ class UserSharedPreferences(context: Context) {
     }
 
     fun setBoolean(key: String, value: Boolean) {
-        prefs.edit().putBoolean(key, value).apply()
+        prefs.edit { setBoolean(key, value) }
     }
 
     fun saveUserInformation(
@@ -34,7 +37,37 @@ class UserSharedPreferences(context: Context) {
         setString(KEY_SPECIALTY, user.specialty)
     }
 
-    fun deleteUserInformation() = prefs.edit().clear().apply()
+    fun deleteUserInformation() = prefs.edit { clear() }
+
+
+//    fun getString(key: String, defValue: String?): String {
+//        return prefs.getString(key, defValue).toString()
+//    }
+//
+//    fun setString(key: String, value: String?) {
+//        prefs.edit().putString(key, value).apply()
+//    }
+//
+//    fun getBoolean(key: String, defValue: Boolean): Boolean {
+//        return prefs.getBoolean(key, defValue)
+//    }
+//
+//    fun setBoolean(key: String, value: Boolean) {
+//        prefs.edit().putBoolean(key, value).apply()
+//    }
+//
+//    fun saveUserInformation(
+//        isLogin: Boolean,
+//        user: User
+//    ) {
+//        setBoolean(KEY_ISLOGIN, isLogin)
+//        setString(KEY_ID, user.id)
+//        setString(KEY_PASSWORD, user.password)
+//        setString(KEY_NAME, user.name)
+//        setString(KEY_SPECIALTY, user.specialty)
+//    }
+//
+//    fun deleteUserInformation() = prefs.edit().clear().apply()
 
     companion object {
         private const val KEY_PREFS = "userInfo"
