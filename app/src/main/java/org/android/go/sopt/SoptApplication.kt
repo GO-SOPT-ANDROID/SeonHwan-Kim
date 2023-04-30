314dc1ddf9511e6e2edd6b202c25c0de16e48a3a
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-04-30 17:07:55 +0900

    fix : companion object 위치 수정 #3

--- app/src/main/java/org/android/go/sopt/SoptApplication.kt
+++ app/src/main/java/org/android/go/sopt/SoptApplication.kt
@@ -4,12 +4,12 @@ import android.app.Application
 import org.android.go.sopt.util.UserSharedPreferences
 
 class SoptApplication : Application() {
-    companion object{
-        lateinit var prefs: UserSharedPreferences
-    }
-
     override fun onCreate() {
         prefs = UserSharedPreferences(this)
         super.onCreate()
+    }
+
+    companion object {
+        lateinit var prefs: UserSharedPreferences
     }
 }
