da8aa7a66fea60d9c6413e8dffe1e7940a09626a
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-05-08 17:42:55 +0900

    feat : 조건 만족 시 enabled #7

--- app/src/main/java/org/android/go/sopt/presentation/signup/SignUpActivity.kt
+++ app/src/main/java/org/android/go/sopt/presentation/signup/SignUpActivity.kt
@@ -3,6 +3,9 @@ package org.android.go.sopt.presentation.signup
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.core.widget.doAfterTextChanged
 import org.android.go.sopt.R
 import org.android.go.sopt.data.local.User
 import org.android.go.sopt.databinding.ActivitySignUpBinding
@@ -30,57 +33,58 @@ class SignUpActivity : AppCompatActivity() {
             hideKeyboard(binding.root)
         }
 
+        this.signUpBtnEnabled()
         this.onClickComplete()
     }
 
     private fun onClickComplete() {
         with(binding) {
             btSignupComplete.setOnClickListener {
-                val user = User(
-                    etSignupId.text.toString(),
-                    etSignupPassword.text.toString(),
-                    etSignupName.text.toString(),
-                    etSignupSpecialty.text.toString(),
-                )
-
-                if (user.id?.length !in 6..10) {
-                    showShortToast(getString(R.string.sign_up_id_err_msg))
-                } else if (user.password?.length !in 8..12) {
-                    showShortToast(getString(R.string.sign_up_password_err_msg))
-                } else if (user.name?.isEmpty() == true) {
-                    showShortToast(getString(R.string.sign_up_name_err_msg))
-                } else if (user.specialty?.isEmpty() == true) {
-                    showShortToast(getString(R.string.sign_up_specialty_err_msg))
-                } else {
-                    signUpService.signUp(
-                        RequestSignUpDto(
-                            etSignupId.text.toString(),
-                            etSignupPassword.text.toString(),
-                            etSignupName.text.toString(),
-                            etSignupSpecialty.text.toString()
-                        )
-                    ).enqueue(object : retrofit2.Callback<ResponseSignUpDto> {
-                        override fun onResponse(
-                            call: Call<ResponseSignUpDto>,
-                            response: Response<ResponseSignUpDto>
-                        ) {
-                            if (response.isSuccessful) {
-                                response.body()?.message?.let { showShortToast(it) } ?: getString(R.string.login_success_sign_up_msg)
-                                val intent = Intent(this@SignUpActivity, LoginActivity::class.java)
-                                startActivity(intent)
-                                if (!isFinishing) finish()
-                            } else {
-                                response.body()?.message?.let { showShortToast(it) }
-                            }
+                signUpService.signUp(
+                    RequestSignUpDto(
+                        etSignupId.text.toString(),
+                        etSignupPassword.text.toString(),
+                        etSignupName.text.toString(),
+                        etSignupSpecialty.text.toString()
+                    )
+                ).enqueue(object : retrofit2.Callback<ResponseSignUpDto> {
+                    override fun onResponse(
+                        call: Call<ResponseSignUpDto>, response: Response<ResponseSignUpDto>
+                    ) {
+                        if (response.isSuccessful) {
+                            response.body()?.message?.let { showShortToast(it) }
+                                ?: getString(R.string.login_success_sign_up_msg)
+                            val intent = Intent(this@SignUpActivity, LoginActivity::class.java)
+                            startActivity(intent)
+                            if (!isFinishing) finish()
+                        } else {
+                            response.body()?.message?.let { showShortToast(it) }
                         }
+                    }
 
-                        override fun onFailure(call: Call<ResponseSignUpDto>, t: Throwable) {
-                            t.message?.let { showShortToast(it) }
-                        }
+                    override fun onFailure(call: Call<ResponseSignUpDto>, t: Throwable) {
+                        t.message?.let { showShortToast(it) }
+                    }
 
-                    })
+                })
+            }
+        }
+    }
+
+    private fun signUpBtnEnabled() {
+        with(binding) {
+            listOf(etSignupId, etSignupPassword, etSignupName, etSignupSpecialty).forEach {
+                it.doAfterTextChanged {
+                    btSignupComplete.isEnabled = isAllConditionSatisfy()
                 }
             }
         }
+    }
+
+    private fun isAllConditionSatisfy(): Boolean {
+        return binding.etSignupId.text.length in 6..10 &&
+                binding.etSignupPassword.text.length in 8..12 &&
+                binding.etSignupName.text.isNotBlank() &&
+                binding.etSignupSpecialty.text.isNotBlank()
     }
 }
