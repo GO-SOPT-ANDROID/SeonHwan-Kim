4d79397aabe6a372dbe0ae5e22f5202a1694a058
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-05-11 01:22:43 +0900

    feat : KakaoApiFactory 추가 #7

--- app/src/main/java/org/android/go/sopt/data/remote/ApiFactory.kt
+++ app/src/main/java/org/android/go/sopt/data/remote/ApiFactory.kt
@@ -5,9 +5,11 @@ import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import org.android.go.sopt.BuildConfig
 import org.android.go.sopt.data.remote.service.HomeUserService
+import org.android.go.sopt.data.remote.service.KakaoSearchService
 import org.android.go.sopt.data.remote.service.SignInService
 import org.android.go.sopt.data.remote.service.SignUpService
 import retrofit2.Retrofit
+import retrofit2.create
 
 object ApiFactory {
     val retrofit: Retrofit by lazy {
@@ -32,8 +34,21 @@ object ReqresApiFactory {
     inline fun <reified T> create(): T = retrofit.create<T>(T::class.java)
 }
 
+object KakaoApiFactory{
+    val retrofit: Retrofit by lazy {
+        val json = Json { ignoreUnknownKeys = true }
+        Retrofit.Builder()
+            .baseUrl(BuildConfig.KAKAO_BASE_URL)
+            .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+            .build()
+    }
+
+    inline fun <reified T> create(): T = retrofit.create<T>(T::class.java)
+}
+
 object ServicePool {
     val signUpService = ApiFactory.create<SignUpService>()
     val signInService = ApiFactory.create<SignInService>()
     val getHomeUserService = ReqresApiFactory.create<HomeUserService>()
+    val kakaoSearchService = KakaoApiFactory.create<KakaoSearchService>()
 }
