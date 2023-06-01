abd81215d811e94518ab68d767b27e1950b69b6a
SeonHwan-Kim <rlatjsghks4647@naver.com>
2023-06-01 15:13:59 +0900

    mod : login 연동 #10

--- app/src/main/java/org/android/go/sopt/presentation/login/LoginActivity.kt
+++ app/src/main/java/org/android/go/sopt/presentation/login/LoginActivity.kt
@@ -2,37 +2,26 @@ package org.android.go.sopt.presentation.login
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModel
+import dagger.hilt.android.AndroidEntryPoint
 import org.android.go.sopt.R
+import org.android.go.sopt.databinding.ActivityLoginBinding
 import org.android.go.sopt.presentation.main.MainActivity
 import org.android.go.sopt.presentation.signup.SignUpActivity
-import org.android.go.sopt.databinding.ActivityLoginBinding
-import org.android.go.sopt.SoptApplication
-import org.android.go.sopt.data.remote.ServicePool
-import org.android.go.sopt.data.remote.model.RequestSignInDto
-import org.android.go.sopt.data.remote.model.ResponseSignInDto
-import org.android.go.sopt.data.remote.service.SignInService
-import org.android.go.sopt.util.*
-import retrofit2.Call
-import retrofit2.Response
+import org.android.go.sopt.util.binding.BindingActivity
+import org.android.go.sopt.util.extension.hideKeyboard
+import org.android.go.sopt.util.extension.showShortToast
+import org.android.go.sopt.util.state.UiState.Error
+import org.android.go.sopt.util.state.UiState.Failure
+import org.android.go.sopt.util.state.UiState.Success
 
-
-class LoginActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityLoginBinding
+@AndroidEntryPoint
+class LoginActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_login) {
     private val viewModel by viewModels<LoginViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
         binding.vm = viewModel
-        binding.lifecycleOwner = this
 
         setContentView(binding.root)
 
@@ -47,22 +36,29 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun onClickLogin() {
-        viewModel.onClickLogin()
-        viewModel.signIn.observe(this){ data ->
-            when(data){
-                200 -> navigateToMainActivity()
-                else -> showShortToast("id 또는 password가 일치하지 않습니다.")
+        viewModel.signInState.observe(this) { state ->
+            when (state) {
+                is Success -> {
+                    navigateToMainActivity()
+                }
+
+                is Failure -> {
+                    showShortToast("아이디 혹은 비밀번호가 잘못되었습니다")
+                }
+
+                is Error -> {
+                    showShortToast("문제가 발생하였습니다")
+                }
             }
         }
     }
 
-    private fun navigateToMainActivity(){
+    private fun navigateToMainActivity() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         showShortToast("로그인 성공")
-        if(!isFinishing) finish()
+        if (!isFinishing) finish()
     }
-
 
     private fun onClickSignUp() {
         binding.btMainSignUp.setOnClickListener {
--- app/src/main/java/org/android/go/sopt/presentation/login/LoginViewModel.kt
+++ app/src/main/java/org/android/go/sopt/presentation/login/LoginViewModel.kt
@@ -1,60 +1,68 @@
 package org.android.go.sopt.presentation.login
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import org.android.go.sopt.R
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import org.android.go.sopt.SoptApplication
-import org.android.go.sopt.data.remote.ServicePool
-import org.android.go.sopt.data.remote.model.BaseResponseDto
-import org.android.go.sopt.data.remote.model.RequestSignInDto
-import org.android.go.sopt.data.remote.model.ResponseSignInDto
-import retrofit2.Call
-import retrofit2.Response
+import org.android.go.sopt.data.model.request.RequestSignInDto
+import org.android.go.sopt.domain.repository.AuthRepository
+import org.android.go.sopt.util.state.UiState
+import org.android.go.sopt.util.state.UiState.Error
+import org.android.go.sopt.util.state.UiState.Failure
+import org.android.go.sopt.util.state.UiState.Success
+import retrofit2.HttpException
+import javax.inject.Inject
 
-class LoginViewModel: ViewModel() {
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+    private val authRepository: AuthRepository,
+) : ViewModel() {
     val id = MutableLiveData("")
     val password = MutableLiveData("")
 
-    private val _signIn = MutableLiveData<Int>()
-    val signIn: LiveData<Int> get() = _signIn
+    private val _signInState = MutableLiveData<UiState>()
+    val signInState: LiveData<UiState>
+        get() = _signInState
 
-    fun onClickLogin(){
-        signIn()
-    }
-
-    private fun signIn(){
-        ServicePool.signInService.signIn(
-            RequestSignInDto(
-                id.value.toString(), password.value.toString()
+    fun onClickSignIn() {
+        viewModelScope.launch {
+            authRepository.signIn(
+                RequestSignInDto(
+                    id.value.toString(),
+                    password.value.toString(),
+                ),
             )
-        ).enqueue(object : retrofit2.Callback<BaseResponseDto<ResponseSignInDto>> {
-            override fun onResponse(
-                call: Call<BaseResponseDto<ResponseSignInDto>>,
-                response: Response<BaseResponseDto<ResponseSignInDto>>
-            ) {
-                if (response.isSuccessful) {
+                .onSuccess {
                     SoptApplication.prefs.setBoolean(KEY_ISLOGIN, true)
-                    _signIn.value = response.body()?.status
-                } else {
-                    Log.d("response err", response.body().toString())
+                    _signInState.value = Success
                 }
-            }
+                .onFailure { throwable ->
+                    if (throwable is HttpException) {
+                        when (throwable.code()) {
+                            INVALID_INPUT -> {
+                                _signInState.value = Failure(INVALID_INPUT)
+                            }
 
-            override fun onFailure(call: Call<BaseResponseDto<ResponseSignInDto>>, t: Throwable) {
-                Log.d("errrr", t.message.toString())
-            }
-        })
+                            else -> {
+                                _signInState.value = Error
+                            }
+                        }
+                    }
+                }
+        }
     }
 
     fun autoLogin() {
         if (SoptApplication.prefs.getBoolean(KEY_ISLOGIN, false)) {
-            _signIn.value = 200
+            _signInState.value = Success
         }
     }
 
-    companion object{
+    companion object {
         const val KEY_ISLOGIN = "isLogin"
+        const val INVALID_INPUT = 400
     }
 }
